99b987906673a1415f27318baac91e127b011932
as <as@ass-MacBook-Pro.local>
2017-06-13 11:09:23 +0800

    递归

--- recurrence.py
+++ recurrence.py
@@ -11,3 +11,10 @@ def compare(arg):
 	else:
 		arg.pop()
 		return 1+compare(arg)
+#求列表最大数
+def max(arg):
+	if len(arg)==1:
+		return arg[0]
+	else:
+		arg.pop(0) if arg[0]<arg[1] else arg.pop(1)
+		return max(arg)
